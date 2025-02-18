aa39c1a3278fee4b463457cf3d25b4e80d119c84
SreinkNok <sreeshakulkarni@gmail.com>
2025-02-18 10:08:37 +0530

    v

---
+++ Maggie.java
@@ -0,0 +1,10 @@
+public class Maggie {
+    public static void main(String[] args) {
+        System.out.println("Maggie Recip");
+        Maggie();
+    }
+          public static void Maggie() {
+            System.out.println("Biol The Maggie");
+            System.out.println("Eat the Maggie");
+          }
+}
