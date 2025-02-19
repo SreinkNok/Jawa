7d71580a84a4c9fb6bf9a38cd2389084be48b6ab
SreinkNok <sreeshakulkarni@gmail.com>
2025-02-19 09:48:09 +0530

    sqws

---
+++ Ten10.java
@@ -0,0 +1,13 @@
+public class Ten10 {
+    public static void main(String[] args) {
+        for(int i = 10; 1<i; i--){
+        System.out.println("The Value of I is"+i);
+    }
+    int j = 0;
+    while (j<11){
+        System.out.println("The Value of J " +j);
+         j++;
+    }
+}
+
+}
