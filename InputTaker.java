e72d0708c17ec3670cbdd7763db485f32078910d
SreinkNok <sreeshakulkarni@gmail.com>
2025-02-19 10:00:26 +0530

    u5hh

---
+++ InputTaker.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class InputTaker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Whats Your Name");
+        String x = sc.nextLine();
+        System.out.println("What is your lucky number");
+        int y = sc.nextInt();
+        System.out.println("Hi "+x+"your name is very cringe and average sound auful and you lucky number "+y+" its very unlucky be carfull");
+    }
+}
